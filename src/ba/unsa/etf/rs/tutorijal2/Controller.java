37fc7a028c0e099c123564d9eb613e3dd0627c39
Benjamin Bakija <benjaminbakija20@gmail.com>
2020-03-17 16:17:56 +0100

    Povezan Controller sa sample.fxml Povezan button sa labelom

--- src/ba/unsa/etf/rs/tutorijal2/Controller.java
+++ src/ba/unsa/etf/rs/tutorijal2/Controller.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rs.tutorijal2;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+
+    public Label labalaPrezime;
+
+    public void onClick(ActionEvent actionEvent) {
+        labalaPrezime.setText(labalaPrezime.getText() + labalaPrezime.getText());
+    }
 }
