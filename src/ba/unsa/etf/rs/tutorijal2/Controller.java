8d2ab7700f6450e76b4fe8c1614adfb1458cf14e
Benjamin Bakija <benjaminbakija20@gmail.com>
2020-03-17 17:10:53 +0100

    Dodana slika kao background

--- src/ba/unsa/etf/rs/tutorijal2/Controller.java
+++ src/ba/unsa/etf/rs/tutorijal2/Controller.java
@@ -2,11 +2,13 @@ package ba.unsa.etf.rs.tutorijal2;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
 public class Controller {
 
 
     public Label labalaPrezime;
+    public BorderPane pane;
 
     public void onClick(ActionEvent actionEvent) {
         labalaPrezime.setText(labalaPrezime.getText() + labalaPrezime.getText());
