4830278b6297aba63785dac2bfa3b002e2f5e1a2
Benjamin Bakija <benjaminbakija20@gmail.com>
2020-03-17 15:56:39 +0100

    Napravljeno korisnicko okruzenje

---
+++ src/ba/unsa/etf/rs/tutorijal2/Main.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rs.tutorijal2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+
+public class Main extends Application {
+
+    @Override
+    public void start(@NotNull Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
